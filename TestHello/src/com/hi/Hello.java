6d7b6fc7886c461b4c1c58f200518d5fbf52fa25
zhaoyao <47496838@qq.com>
2020-04-26 15:36:34 +0800

    日期

--- TestHello/src/com/hi/Hello.java
+++ TestHello/src/com/hi/Hello.java
@@ -1,9 +1,18 @@
 package com.hi;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import org.eclipse.jdt.internal.compiler.batch.Main;
 
 public class Hello {
 public static void main(String[] args) {
 	System.out.println("Hello111");
+	Calendar ca=Calendar.getInstance();
+	System.out.println(ca.get(Calendar.MONTH));
+	ca.add(Calendar.MONTH, 1);
+	ca.set(Calendar.DAY_OF_MONTH,0);
+	System.out.println(new SimpleDateFormat("yyyy-MM-dd").format(ca.getTime()));
+	System.out.println(ca.get(Calendar.DAY_OF_MONTH));
 }
 }
