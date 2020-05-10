622b818fb3c1581232bee58e6530fb5f136e1e7f
zhaoyao <474968838@qq.com>
2020-05-10 15:39:29 +0800

    sd

--- TestHello/src/com/hi/HelloWorld.java
+++ TestHello/src/com/hi/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       System.out.println("我不是测试0510");
-       System.out.println("我什么大苏打，那怎么办");
+       System.out.println("我什么大苏打，那1怎么办");
        System.out.println("我是分支other1");
 	}
 
