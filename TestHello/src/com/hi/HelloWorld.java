4ec125631858f1675eeb15eee65e07af232ff4fe
Never Ends <57120034+sunshine-zhao@users.noreply.github.com>
2020-05-10 14:53:29 +0800

    Update HelloWorld.java

--- TestHello/src/com/hi/HelloWorld.java
+++ TestHello/src/com/hi/HelloWorld.java
@@ -4,7 +4,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-      System.out.println("我是测试0510");
+      System.out.println("我不是测试0510");
+       System.out.println("我什么也不会");
 	}
 
 }
